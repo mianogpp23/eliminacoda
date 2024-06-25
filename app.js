e54942b6bef6dfb2c347c992fc0210eab9b792ba
mianogpp23 <mianopino@libero.it>
2024-06-25 06:26:12 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const http = require('http');
+const socketIo = require('socket.io');
+
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 
 // Middleware per gestire il parsing dei dati del form
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Middleware per gestire i file statici
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Definizione dei valori iniziali per ogni sala
 let initialValue1 = 0;
 let initialValue2 = 0;
 let initialValue3 = 0;
 let initialValue4 = 0;
+
+// Impostazione del motore di rendering EJS
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 // Percorso per il rendering della pagina index.ejs
 app.get('/', (req, res) => {
@@ -67,16 +79,20 @@ app.post('/remote', (req, res) => {
       break;
   }
 
+  // Emissione dei nuovi valori ai client connessi
+  io.emit('updateValues', {
+    initialValue1: initialValue1,
+    initialValue2: initialValue2,
+    initialValue3: initialValue3,
+    initialValue4: initialValue4
+  });
+
   // Redirect alla pagina del telecomando dopo l'aggiornamento dei valori
   res.redirect('/remote');
 });
 
-// Impostazione del motore di rendering EJS
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
 // Avvio del server sulla porta 3000
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
